45d9085dac8a67a2e67b416e387b67f21f687d0b
Your Name <you@example.com>
2021-11-28 15:18:41 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::net::UdpSocket;
+
+use quiche;
+use smol::Async;
+
+pub struct QuicSocket {
+    config: quiche::Config,
+    socket: Async<UdpSocket>,
+}
+
+impl QuicSocket {
+    pub fn new() -> Result<Self> {
+        let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION)?;
+        config.set_cc_algorithm_name("reno")?;
+
+        let mut socket = Async::<UdpSocket>::bind("")?;
+
+        Self { config, socket }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
